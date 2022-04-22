8564da6280d65395fe58e3e77d419ae955b239e5
MacKenzie Kerwin <kerwin.m@husky.neu.edu>
2022-04-22 10:38:01 -0400

    more content

--- src/images/index.js
+++ src/images/index.js
@@ -1,11 +1,19 @@
 import WCCo1800s from './waterbury_1800s.jpeg';
 import Map1899 from './map-1899.jpg';
 import Sylvan15 from './15-sylvan.jpg';
+import Church from './immaculate-conception.jpeg';
+import WtbyBrass from './wtby-brass.jpeg';
+import WtbyBrassMills from './wtby-brass-mills.jpg';
+import Clocks from './clocks.jpeg';
 
 const images = {
   wbcco: WCCo1800s,
   map1899: Map1899,
   sylvan15: Sylvan15,
+  church: Church,
+  wtbyBrass: WtbyBrass,
+  wtbyBrassMills: WtbyBrassMills,
+  clocks: Clocks,
 };
 
 export default images;
